15202423d314b6d103a8fdddc987e5b1d0942786
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-10 18:34:58 -0800

    Update api_keys.py

--- mainfiles/api_keys.py
+++ mainfiles/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f82e84042ecb6faa5a7de708ab1ad1fd"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyD0JOXtW6jlux8dEcP9oeJe_PtL8-M6Dgo"
+g_key = "o"
